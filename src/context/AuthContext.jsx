b19363cd960ce84d5f12af218a88cae5f3d6ae25
avnms <avnmsarma@gmail.com>
2024-08-05 22:15:28 +1000

    Update AuthContext to use jwt

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,15 +1,26 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { jwtDecode } from "jwt-decode";
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
 
+    useEffect(() => {
+        const token = localStorage.getItem("access_token");
+        if (token) {
+            const decoded = jwtDecode(token);
+            setUser({ username: decoded.username });
+        }
+    }, []);
+
     const login = (userData) => {
         setUser(userData);
     };
 
     const logout = () => {
+        localStorage.removeItem("access_token");
+        localStorage.removeItem("refresh_token");
         setUser(null);
     };
 
