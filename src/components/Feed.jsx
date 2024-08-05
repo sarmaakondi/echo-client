6944d19e941fececd727fc35d7eea03c876ded03
avnms <avnmsarma@gmail.com>
2024-08-05 20:57:07 +1000

    Update feed component with placeholder data and move the required CSS rules from hero to feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,5 +1,25 @@
+import "./Feed.css";
+
 const Feed = () => {
-    return <h1>Feed</h1>;
+    return (
+        <div className="child-container">
+            <div>
+                <div className="profile-header">
+                    <div className="profile-container">
+                        <img className="profile-picture" src="/favicon.png" />
+                        <p className="username">Sarma Akondi</p>
+                    </div>
+                    <button id="post-button">Echo</button>
+                </div>
+                <input
+                    type="text"
+                    name="echo-text"
+                    id="echo-text"
+                    placeholder="Start an echo..."
+                />
+            </div>
+        </div>
+    );
 };
 
 export default Feed;
