cb11a6eed48f2a277a9b7b895f32ff69bf933742
avnms <avnmsarma@gmail.com>
2024-08-05 17:19:14 +1000

    Wrap the navbar icons with Link to convert them to anchor tags

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,22 +1,34 @@
+import { Link } from "react-router-dom";
+
 import "./NavBar.css";
 
 const NavBar = () => {
     return (
         <div className="nav-container">
             <div className="nav-icon">
-                <i className="fa-solid fa-house"></i>
+                <Link to="/home">
+                    <i className="fa-solid fa-house"></i>
+                </Link>
             </div>
             <div className="nav-icon">
-                <i className="fa-solid fa-magnifying-glass"></i>
+                <Link to="/search">
+                    <i className="fa-solid fa-magnifying-glass"></i>
+                </Link>
             </div>
             <div className="nav-icon">
-                <i className="fa-solid fa-plus"></i>
+                <Link to="/create">
+                    <i className="fa-solid fa-plus"></i>
+                </Link>
             </div>
             <div className="nav-icon">
-                <i className="fa-regular fa-heart"></i>
+                <Link to="/favorites">
+                    <i className="fa-regular fa-heart"></i>
+                </Link>
             </div>
             <div className="nav-icon">
-                <i className="fa-regular fa-user"></i>
+                <Link to="/profile">
+                    <i className="fa-regular fa-user"></i>
+                </Link>
             </div>
         </div>
     );
