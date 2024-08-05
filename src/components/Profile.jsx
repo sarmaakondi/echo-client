0dda5ddc249111ce998877dd2961cc644f75b742
avnms <avnmsarma@gmail.com>
2024-08-05 21:53:29 +1000

    Render Login and Logout dynamically

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,12 @@
+import { AuthContext } from "../context/AuthContext";
+import { useContext } from "react";
+
 import Login from "./Login";
 
 const Profile = () => {
-    return <Login />;
+    const { user } = useContext(AuthContext);
+
+    return user ? <p>Logged in</p> : <Login />;
 };
 
 export default Profile;
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-import { AuthProvider } from "./context/AuthContext.js";
+import { AuthProvider } from "./context/AuthContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <AuthProvider>
