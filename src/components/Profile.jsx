14d02e54c28caf12765be3a3c9c9e2a400e7eb9c
avnms <avnmsarma@gmail.com>
2024-08-06 10:31:09 +1000

    Add app name and log

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,11 +2,20 @@ import { AuthContext } from "../context/AuthContext";
 import { useContext } from "react";
 
 import Login from "./Login";
+import "./Profile.css";
 
 const Profile = () => {
     const { user } = useContext(AuthContext);
 
-    return user ? <p>Logged in</p> : <Login />;
+    return (
+        <div className="profile-container">
+            <div className="app-logo">
+                <img src="/favicon.png" alt="logo" id="logo" />
+                <h1 id="app-name">Echo</h1>
+            </div>
+            {user ? <p>Logged in</p> : <Login />}
+        </div>
+    );
 };
 
 export default Profile;
