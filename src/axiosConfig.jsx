2bf22b1162e7f61f4a1c9a786caf7e2eeb5fbaa8
avnms <avnmsarma@gmail.com>
2024-08-07 10:58:13 +1000

    Remove identified reduntant logic

--- src/axiosConfig.jsx
+++ src/axiosConfig.jsx
@@ -26,27 +26,6 @@ const setupInterceptors = (refreshAccessToken) => {
             return Promise.reject(error);
         }
     );
-
-    axiosInstance.interceptors.response.use(
-        (response) => response,
-        async (error) => {
-            const originalRequest = error.config;
-            if (error.response.status === 401 && !originalRequest._retry) {
-                originalRequest._retry = true;
-                try {
-                    await refreshAccessToken();
-                    const newToken = localStorage.getItem("access_token");
-                    axios.defaults.headers.common[
-                        "Authorization"
-                    ] = `Bearer ${newToken}`;
-                    return axiosInstance(originalRequest);
-                } catch (refreshError) {
-                    return Promise.reject(refreshError);
-                }
-            }
-            return Promise.reject(error);
-        }
-    );
 };
 
 export { axiosInstance, setupInterceptors };
