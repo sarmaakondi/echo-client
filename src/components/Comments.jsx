849f19cc49a5e5e8cc6abf5595a4b1794d8bc12a
avnms <avnmsarma@gmail.com>
2024-08-08 07:35:57 +1000

    Update comments layout and add transition effect

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -2,16 +2,27 @@ import "./Comments.css";
 
 const Comments = ({ echo, showComments }) => {
     return (
-        <div key={echo.id}>
+        <div key={echo.id} className={`comment ${showComments ? "show" : ""}`}>
             {(echo.comments ?? []).map((comment) => (
-                <div key={comment.id} className="comment">
+                <div key={comment.id}>
                     {showComments && (
-                        <p>
-                            <span className="feed-comment-user">
-                                {comment.user}:{" "}
-                            </span>
-                            {comment.content}
-                        </p>
+                        <div className="comment-container">
+                            <div>
+                                <img
+                                    className="comment-profile-picture"
+                                    src="profile.png"
+                                    alt="user profile picture"
+                                />
+                            </div>
+                            <div>
+                                <p className="comment-username">
+                                    {comment.user}
+                                </p>
+                                <p className="comment-content">
+                                    {comment.content}
+                                </p>
+                            </div>
+                        </div>
                     )}
                 </div>
             ))}
