9357856804db54b0dcd0e109f9ee9617aa3f8849
avnms <avnmsarma@gmail.com>
2024-08-07 18:08:10 +1000

    Fix the height of the feed container to show the missing scroll bar

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,16 +1,19 @@
 import "./Comments.css";
 
 const Comments = ({ echo }) => {
+    let showComments = true;
     return (
         <div key={echo.id}>
             {(echo.comments ?? []).map((comment) => (
                 <div key={comment.id} className="comment">
-                    <p>
-                        <span className="feed-comment-user">
-                            {comment.user}:{" "}
-                        </span>
-                        {comment.content}
-                    </p>
+                    {showComments && (
+                        <p>
+                            <span className="feed-comment-user">
+                                {comment.user}:{" "}
+                            </span>
+                            {comment.content}
+                        </p>
+                    )}
                 </div>
             ))}
         </div>
