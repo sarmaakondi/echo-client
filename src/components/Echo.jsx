b6ea42467f2ebdea519046dfe6daf1d5afc2fdd9
avnms <avnmsarma@gmail.com>
2024-08-07 15:06:34 +1000

    Split Echo logic to its own component

--- src/components/Echo.jsx
+++ src/components/Echo.jsx
@@ -1,5 +1,35 @@
+import { AuthContext } from "../context/AuthContext";
+import { useContext } from "react";
+
+import "./Echo.css";
+
 const Echo = () => {
-    return <h1>Echo</h1>;
+    const { user } = useContext(AuthContext);
+
+    return (
+        <>
+            {user && (
+                <div className="profile-parent-container">
+                    <div className="profile-header">
+                        <div className="echo-profile-container">
+                            <img
+                                className="profile-picture"
+                                src="/profile.png"
+                            />
+                            <p className="username">{user["username"]}</p>
+                        </div>
+                        <button id="post-button">Echo</button>
+                    </div>
+                    <input
+                        type="text"
+                        name="echo-text"
+                        id="echo-text"
+                        placeholder="Start an echo..."
+                    />
+                </div>
+            )}
+        </>
+    );
 };
 
 export default Echo;
--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,33 +1,11 @@
-import { AuthContext } from "../context/AuthContext";
-import { useContext } from "react";
-
+import Echo from "./Echo";
 import "./Feed.css";
 
 const Feed = ({ echoes }) => {
-    const { user } = useContext(AuthContext);
-
     return (
         <div className="child-container">
-            {user && (
-                <div className="profile-parent-container">
-                    <div className="profile-header">
-                        <div className="echo-profile-container">
-                            <img
-                                className="profile-picture"
-                                src="/profile.png"
-                            />
-                            <p className="username">{user["username"]}</p>
-                        </div>
-                        <button id="post-button">Echo</button>
-                    </div>
-                    <input
-                        type="text"
-                        name="echo-text"
-                        id="echo-text"
-                        placeholder="Start an echo..."
-                    />
-                </div>
-            )}
+            {/* Echo form with user profile */}
+            <Echo />
 
             {/* Render Feed */}
             <div className="feed">
