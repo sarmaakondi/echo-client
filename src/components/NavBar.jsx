698008b28752a669d894cf863ac7c0982c5f8796
avnms <avnmsarma@gmail.com>
2024-08-08 12:21:58 +1000

    Identify active link and apply required CSS rules to highlight it visually

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,35 +1,54 @@
-import { Link } from "react-router-dom";
-
+import { NavLink } from "react-router-dom";
 import "./NavBar.css";
 
 const NavBar = () => {
     return (
         <div className="nav-container">
-            <Link to="/feed" className="nav-link">
+            <NavLink
+                to="/feed"
+                className={({ isActive }) =>
+                    isActive ? "nav-link active-link" : "nav-link"
+                }>
                 <div className="nav-icon">
                     <i className="fa-solid fa-house"></i>
                 </div>
-            </Link>
-            <Link to="/search" className="nav-link">
+            </NavLink>
+            <NavLink
+                to="/search"
+                className={({ isActive }) =>
+                    isActive ? "nav-link active-link" : "nav-link"
+                }>
                 <div className="nav-icon">
                     <i className="fa-solid fa-magnifying-glass"></i>
                 </div>
-            </Link>
-            <Link to="/create" className="nav-link">
+            </NavLink>
+            <NavLink
+                to="/create"
+                className={({ isActive }) =>
+                    isActive ? "nav-link active-link" : "nav-link"
+                }>
                 <div className="nav-icon">
                     <i className="fa-solid fa-plus"></i>
                 </div>
-            </Link>
-            <Link to="/favorites" className="nav-link">
+            </NavLink>
+            <NavLink
+                to="/favorites"
+                className={({ isActive }) =>
+                    isActive ? "nav-link active-link" : "nav-link"
+                }>
                 <div className="nav-icon">
                     <i className="fa-regular fa-heart"></i>
                 </div>
-            </Link>
-            <Link to="/profile" className="nav-link">
+            </NavLink>
+            <NavLink
+                to="/profile"
+                className={({ isActive }) =>
+                    isActive ? "nav-link active-link" : "nav-link"
+                }>
                 <div className="nav-icon">
                     <i className="fa-regular fa-user"></i>
                 </div>
-            </Link>
+            </NavLink>
         </div>
     );
 };
